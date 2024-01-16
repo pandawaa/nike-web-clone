05c24f482809ca2265059c07cbd0b8e3941944ca
pandawaa <pallakangoding@gmail.com>
2024-01-16 13:35:11 +0800

    feat: Special Offer

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button"
 import Navbar from "./Navbar"
 import ShoeCard from "./ShoeCard"
 import PopularProductCard from "./PopularProductCard"
+import ServiceCard from "./ServiceCard"
 
-export { Button, Navbar, ShoeCard, PopularProductCard }
+export { Button, Navbar, ShoeCard, PopularProductCard, ServiceCard }
