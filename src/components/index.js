60a99123419685d7ecb6e93dbc9ed6891eecf4ed
pandawaa <pallakangoding@gmail.com>
2024-01-16 13:52:17 +0800

    feat: CustomerReviews

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Navbar from "./Navbar"
 import ShoeCard from "./ShoeCard"
 import PopularProductCard from "./PopularProductCard"
 import ServiceCard from "./ServiceCard"
+import ReviewCard from "./ReviewCard"
 
-export { Button, Navbar, ShoeCard, PopularProductCard, ServiceCard }
+export { Button, Navbar, ShoeCard, PopularProductCard, ServiceCard, ReviewCard }
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,32 @@
+import { ReviewCard } from "../components"
+import { reviews } from "../constants"
+
 const CustomerReviews = () => {
-  return <div>CustomerReviews</div>
+  return (
+    <section className="max-container">
+      <h3 className="text-center font-palanquin text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="m-auto mt-4 max-w-lg text-center info-text pb-5">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us
+      </p>
+
+      <div>
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default CustomerReviews
